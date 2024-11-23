7085e5d7f7dbfc6361028cc89e83ad6affadb0c5
przemekwrona <przemekwrona25@gmail.com>
2024-11-23 16:02:51 +0100

    feat: GET exchange from NBP Api

--- src/main/java/pl/wrona/insurance/client/NbpClient.java
+++ src/main/java/pl/wrona/insurance/client/NbpClient.java
@@ -2,9 +2,9 @@ package pl.wrona.insurance.client;
 
 import jakarta.annotation.Resource;
 import org.springframework.cloud.openfeign.FeignClient;
-import pl.wrona.insurance.api.ExchangeApi;
+import pl.wrona.nbp.api.ExchangeratesApi;
 
 @Resource
 @FeignClient(value = "${nbp.client.name}", url = "${nbp.client.url}")
-public interface NbpClient extends ExchangeApi {
+public interface NbpClient extends ExchangeratesApi {
 }
